78910ef3f11fbc7aa1d493011928ae9dbcdbe028
AloneAc <113434837+AloneAc@users.noreply.github.com>
2022-09-22 21:41:21 +0800

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,6 @@
+const { Client } = require('discord.js');
+const ytdl = require('ytdl-core');
+const { token } = require('./token.json');
+const { prefix } = require('./config.json');
+const client = new Client();
+
